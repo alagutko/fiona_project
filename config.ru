3cae985e5cec1c8bddcfb78e484168be1db2ae27
Alexander Lagutko <alagutko@ampsoft.by>
2014-09-16 13:12:53 +0200

    text

--- config.ru
+++ config.ru
@@ -1,4 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
-run PartnerApp::Application
+map ActionController::Base.config.relative_url_root || "/" do
+  run PartnerApp::Application
+end
--- config/environments/preview.rb
+++ config/environments/preview.rb
@@ -1,7 +1,6 @@
 PartnerApp::Application.configure do
-  config.action_controller.relative_url_root = "/live"
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_controller.relative_url_root = "/default"
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -16,7 +15,7 @@ PartnerApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -65,4 +64,6 @@ PartnerApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  RailsConnector::Configuration.mode = "editor"
 end
