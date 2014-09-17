560f1e895ca32b06408176270e14c0f24c10652a
Alexander Lagutko <alagutko@ampsoft.by>
2014-09-17 09:01:04 +0200

    s

--- app/helpers/blog_entry_helper.rb
+++ app/helpers/blog_entry_helper.rb
@@ -1,2 +1,11 @@
 module BlogEntryHelper
+
+	def readable_author(blog_entry)
+		name = blog_entry.readbale_author
+		if Rails.env == "preview"
+		  name = "#{name} (#{display_field(blog_entry, ::author)})"
+		  return name.html_safe
+		end
+	end
+
 end
