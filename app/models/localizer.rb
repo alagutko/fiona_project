9aef78e277d5b720cfe25e0bc78daa375a3d650d
Alexander Lagutko <sir_mendeleev@mail.ru>
2014-09-17 12:56:53 +0400

    ddd

--- app/models/localizer.rb
+++ app/models/localizer.rb
@@ -1,3 +1,9 @@
 class Localizer < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  def self.detect_locale(obj, request)
+    if obj.present? && obj.mime_type = "text/html" && obj.homepage.respond_to?('name')
+      return obj.homepage.name.to_sym
+    end
+  end
 end
