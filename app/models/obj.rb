332f41f20bd9b0526a00a737dd88aae6ac28e923
Alexander Lagutko <sir_mendeleev@mail.ru>
2014-09-17 12:28:22 +0400

    ddd

--- app/models/obj.rb
+++ app/models/obj.rb
@@ -2,4 +2,9 @@
 # See the documentation for details: http://rubydoc.info/gems/infopark_fiona_connector/RailsConnector/BasicObj
 class Obj < RailsConnector::BasicObj
   # Your custom code goes here...
+
+
+  def permitted_for_user?(user)
+    return true
+  end
 end
