59df78a670bc47bfee47006672835bea76ec6584
Alexander Lagutko <alagutko@ampsoft.by>
2014-09-17 09:21:42 +0200

    s

--- app/assets/javascripts/blog_folder.js
+++ app/assets/javascripts/blog_folder.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function isMemeberOfAdmin(){
+	alert(inline_editor.currentUserIsMemeberOf("admins") ? "yes" : "no");
+	return false;
+}
+
+function hasPermissionsGlobalRoot(){
+alert(inline_editor.currentUserHasGlobalPermission(permissionGlobalRoot) ? yes : no);
+return false;
+}
+
+$().ready(function(){
+	$('#isMemberOfAdmins').click(isMemeberOfAdmin);
+}
+);
+
+
